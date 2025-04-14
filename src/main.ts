7dffb8e46633c6f3f5321c51f4703cd75584758c
ZerGio23 <sergio.apaza1432@gmail.com>
2025-04-14 15:43:41 -0400

    feat (common): add staged branch

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
+    .setTitle('Cats example 1')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
