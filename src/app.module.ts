2b56d252b5bdf5570943c0c76db812d3aa0d0d7b
ZerGio23 <sergio.apaza1432@gmail.com>
2025-05-08 02:24:52 -0400

    feat: add search places crud and search by category endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SearchPlacesModule } from './search-places/search-places.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? ''),
+    SearchPlacesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
