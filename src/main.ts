5a47bc33171b0dbf01324d80b4dad84906658ea5
yisusMcOdE <65510576+yisusMcOdE@users.noreply.github.com>
2025-06-04 16:00:20 -0400

    fix(endpoint waitlist)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'locahttp://localhost:3000',
+    origin: (process.env.FRONTEND_URL ?? 'http://localhost:3000').split(','),
   });
   const config = new DocumentBuilder()
     .setTitle('AndiGO API')
--- src/waitlist-numbers/create-waitlist-number.dto.ts
+++ src/waitlist-numbers/create-waitlist-number.dto.ts
@@ -3,8 +3,8 @@ import { IsNotEmpty, Matches } from 'class-validator';
 export class CreateWaitlistNumberDto {
   @IsNotEmpty({ message: 'El número no puede estar vacío.' })
   @Matches(
-    /^[0-9]{8}$/,  // Ajusta la expresión según tus necesidades
-    { message: 'El número no tiene un formato válido (8 dígitos).' }
+    /^\+591\d{8}$/,  // Ajusta la expresión según tus necesidades
+    { message: 'El número no tiene un formato válido (12 dígitos).' }
   )
   number: string;
 }
